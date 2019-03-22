bfcf06ea31a56ddbf3d5c79c243f4e68b92dd14e
Alan Houston <alanhouston5000@gmail.com>
2019-03-21 19:44:24 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import Dashboard from "./components/Dashboard"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+let Tables = () =><div>Tables Stuff</div>
+let Charts = () =><div>Charts Stuff</div>
+//these are components!
+
+let Things = () =><div>Things Stuff</div>
 
 function App() {
   return (
+    <Router>
     <div> 
       <div id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -12,10 +21,16 @@ function App() {
         </nav>
         <div style={{backgroundColor: "white"}}>
           {/* PUT YOUR ROUTES HERE */}
+
+          <Route path="/" exact component={Dashboard} />
+          {/* need to have exact^^^ can use in all Routes */}
+          <Route path="/charts" component={Charts} />
+          <Route path="/tables" component={Tables} />
+          {/* the Routes are the if statements! */}
         </div>
       </div>
     </div>
-
+    </Router>
   );
 }
 
